10b864d2d6c672217585206ce67a151f6bbe58f9
GuilhermeFD19 <guii.doreto@gmail.com>
2022-04-13 00:12:42 -0300

    feat: adicionando o search e page-template

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { Header } from "../components";
+import { Header } from "@/components";
 
 const Home: NextPage = () => {
   return (
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
 
       <Header />
 
-      
+
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
