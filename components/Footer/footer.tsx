5c290127eeeb433fdab47010e5944764d8b03d8f
GuilhermeFD19 <guii.doreto@gmail.com>
2022-04-13 09:30:08 -0300

    feat: footer page

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -1,8 +1,11 @@
-export function Footer() {
-    return (
-        <footer>
-           <p>Imersão FrontExpert</p> 
+import Styles from "./footer.module.css";
 
-        </footer>
-    );
+export function Footer() {
+  return (
+    <footer>
+      <div className={Styles.footer}>
+        <p>@ 2022 - Imersão FrontExpert</p>
+      </div>
+    </footer>
+  );
 }
